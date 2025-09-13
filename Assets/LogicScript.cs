ce507e5524429412f068f6afb81ea882740ede0e
AswathiVenkatesan <aswathi9944@gmail.com>
2025-09-13 09:40:45 +0530

    Initial commit

---
+++ Assets/LogicScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore;
+    public Text scoreText;
+    public GameObject gameOverScreen;
+    public AudioSource music;
+
+    [ContextMenu("Increase Score")]
+    public void addScore(int scoreToAdd)
+    {
+        playerScore = playerScore + scoreToAdd;
+        scoreText.text = playerScore.ToString();
+        music.Play();
+    }
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+
+    
+
+    }
+
+
+}
